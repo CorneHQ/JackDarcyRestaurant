a5bdb1ff438a9a59721427e3dc4d109943f30b16
CorneHQ <cornedev@outlook.com>
2021-03-01 13:48:25 +0100

    [FEATURE] Added main files for CSharp project

---
+++ Jack Darcy Restaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jack_Darcy_Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
