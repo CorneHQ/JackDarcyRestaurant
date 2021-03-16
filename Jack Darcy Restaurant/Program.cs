2498ce83ead7c93cf06934b196176da412c6491d
DeSadGuy <1020159@hr.nl>
2021-03-16 17:56:34 +0100

    Update Program.cs

--- Jack Darcy Restaurant/Program.cs
+++ Jack Darcy Restaurant/Program.cs
@@ -7,25 +7,30 @@ namespace Jack_Darcy_Restaurant
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string x = "     "; 
-            Console.WriteLine("devin was hier");
             Console.WriteLine("\n\n");
 
-            Console.WriteLine($"{x}____________\n{x}Leuke Header\n\n\n");
+            Console.WriteLine($"     ____________\n      Leuke Header\n\n\n");
             Console.WriteLine($"    Openings tijden\n\n" +
                 $"Maandag   12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"dinsdag   12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"woensdag  12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"donderdag 12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"vrijdag   12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"zaterdag  12:00 - 16:00 and 18:00 - 00:00\n" +
-                $"zondag    12:00 - 16:00 and 18:00 - 00:00\n" +              
+                $"Dinsdag   12:00 - 16:00 and 18:00 - 00:00\n" +
+                $"Woensdag  12:00 - 16:00 and 18:00 - 00:00\n" +
+                $"Donderdag 12:00 - 16:00 and 18:00 - 00:00\n" +
+                $"Vrijdag   12:00 - 16:00 and 18:00 - 00:00\n" +
+                $"Zaterdag  12:00 - 16:00 and 18:00 - 00:00\n" +
+                $"Zondag    12:00 - 16:00 and 18:00 - 00:00\n" +              
                 $"\n\n" +
                 $"    Locatie   \n\n" +
-                $"Wijnhaven 107" +
-                $"3011 WN , Rotterdam" +
-                $"The Netherlands" +
-                $"" +
+                $"Wijnhaven 107\n" +
+                $"3011 WN, Rotterdam\n" +
+                $"The Netherlands\n" +
+                $"\n\n" +
+                $"Contact \n" +
+                $"Telefoon-nummer   : 010 0000000\n" +
+                $"E-mail            : hogeschool@hr.nl \n" +
+                $"Facebook          : https://www.facebook.com \n" +
+                $"Instagram         : https://www.instagram.com \n" +
+                $"Twitter           : https://twitter.com/ \n" +
+                $"pinkterest        : https://www.pinterest.com \n" +
                 $"");
 
         }
