4ffd3ed1eec9dd6ac4b946b4752af3ad0dbc4c68
DeSadGuy <1020159@hr.nl>
2021-03-16 15:37:50 +0100

    openings tijden

--- Jack Darcy Restaurant/Program.cs
+++ Jack Darcy Restaurant/Program.cs
@@ -7,6 +7,21 @@ namespace Jack_Darcy_Restaurant
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string x = "     "; 
+            Console.WriteLine("devin was hier");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine($"{x}____________\n{x}Leuke Header\n\n\n");
+            Console.WriteLine($"    openings tijden\n\n" +
+                $"Maandag   12:00 - 12:00\n" +
+                $"dinsdag   12:00 - 12:00\n" +
+                $"woensdag  12:00 - 12:00\n" +
+                $"donderdag 12:00 - 12:00\n" +
+                $"vrijdag   12:00 - 12:00\n" +
+                $"zaterdag  12:00 - 12:00\n" +
+                $"zondag    12:00 - 12:00\n" +
+                $"");
+
         }
     }
 }
