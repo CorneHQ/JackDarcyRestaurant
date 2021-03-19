8f9771c118dee7fc3e78c4955e4a43c9fdb3206d
DeSadGuy <1020159@hr.nl>
2021-03-19 15:54:23 +0100

    Update Program.cs

--- Jack Darcy Restaurant/Program.cs
+++ Jack Darcy Restaurant/Program.cs
@@ -7,9 +7,9 @@ namespace Jack_Darcy_Restaurant
         static void Main(string[] args)
         {
             Console.WriteLine($"" +
-                $"              _______________\n" +
-                $"              Opening Hours\n"+
-                $"                       \n" +
+                $"              _____________   \n" +
+                $"              Opening Hours   \n"+
+                $"                              \n" +
                 $"Monday                12:00 - 16:00 and 18:00 - 00:00\n" +
                 $"Tuesday               12:00 - 16:00 and 18:00 - 00:00\n" +
                 $"Wednesday             12:00 - 16:00 and 18:00 - 00:00\n" +
@@ -18,16 +18,17 @@ namespace Jack_Darcy_Restaurant
                 $"Saturday              12:00 - 16:00 and 18:00 - 00:00\n" +
                 $"Sunday                12:00 - 16:00 and 18:00 - 00:00\n" +              
                 $"\n\n" +
-                $"                  ________\n" +
-                $"                  Location   \n" +
-                $"                      \n" +
+                $"                  ________    \n" +
+                $"                  Location    \n" +
+                $"                              \n" +
                 $"Address                     : Wijnhaven 107\n" +
                 $"ZipCode                     : 3011 WN, Rotterdam\n" +
                 $"Country                     : The Netherlands\n" +
                 $"\n\n" +
                 $"             _________________\n" +
-                $"             Travel Directions\n\n" +
-                $"The location is near subway station Blaak.     \n" +
+                $"             Travel Directions\n" +
+                $"                              \n" +
+                $"The location is near subway station Blaak.    \n" +
                 $"If you get off at station Blaak and walk      \n" +
                 $"towards station Beurs via Hoogstraat. You     \n" +
                 $"will pass by McDonald at some point. At the   \n" +
@@ -35,14 +36,15 @@ namespace Jack_Darcy_Restaurant
                 $"until you see a bridge and turn right before  \n" +
                 $"the bridge.                                   \n" +
                 $"\n\n" +
-                $"                  _______\n" +
-                $"                  Contact\n" +
-                $"                  \n" +
-                $"Telephone Number          : 010 0000000\n" +
-                $"E-mail                    : hogeschool@hr.nl \n" +
-                $"Facebook                  : https://www.facebook.com \n" +
+                $"                  _______ \n" +
+                $"                  Contact \n" +
+                $"                          \n" +
+                $"Telephone Number          : 010 0000000               \n" +
+                $"Number for foreigners     : +3100 0 0000000           \n" +
+                $"E-mail                    : hogeschool@hr.nl          \n" +
+                $"Facebook                  : https://www.facebook.com  \n" +
                 $"Instagram                 : https://www.instagram.com \n" +
-                $"Twitter                   : https://twitter.com/ \n" +
+                $"Twitter                   : https://twitter.com/      \n" +
                 $"Pinterest                 : https://www.pinterest.com \n" +
                 $"");
 
