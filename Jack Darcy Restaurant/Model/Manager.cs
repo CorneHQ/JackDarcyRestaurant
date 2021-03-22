92caf9e63c708a6d2c3fd987b7acac7c30c7fe11
King0nameless <32332344+King0nameless@users.noreply.github.com>
2021-03-22 01:14:27 +0100

    login works
    login work
    manager global class
    added validation in user model

---
+++ Jack Darcy Restaurant/Model/Manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack_Darcy_Restaurant.Model
+{
+    class Manager
+    {
+        public static int Role_id { get; set; }
+    }
+}
---
+++ Jack Darcy Restaurant/Model/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack_Darcy_Restaurant.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Role_Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(int cId, string cName, string cPassword, string cEmail, int cRoles)
+        {
+            Id = cId;
+            Name = cName;
+            Password = cPassword;
+            Email = cEmail;
+            Role_Id = cRoles;
+        }
+
+        public bool Validate(string vName, string vPassword)
+        {
+            if (Name == vName && Password == vPassword)
+            {
+                Manager.Role_id = Role_Id;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
