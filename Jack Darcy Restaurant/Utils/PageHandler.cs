65614afbf6b42aabe13791ffbd6d3f3c2e9858cf
NBLBN <0934803@hr.nl>
2021-03-23 15:04:44 +0100

    PageHandler

---
+++ Jack Darcy Restaurant/Utils/PageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Jack_Darcy_Restaurant.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack_Darcy_Restaurant.Utils
+{
+    public class PageHandler
+    {
+        public static void switchPage(int newPage)
+        {
+            Console.Clear();
+            if (newPage == 0)
+            {
+                About.Show();
+            } else if  (newPage == 1)
+            {
+                Console.WriteLine("Login Comes Here");
+            } else if (newPage == 2)
+            {
+                Console.WriteLine("Register Comes Here");
+            } else if (newPage == 3) 
+            {
+                MainMenu.Show();
+            } else
+            {
+                Console.WriteLine("\n");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("The page does not exists!");
+                Console.ResetColor();
+                Console.WriteLine("\n");
+                MainMenu.Show();
+            } 
+        }
+    }
+}
