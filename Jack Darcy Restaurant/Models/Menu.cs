15f4e8d5148849a93d4daf9cf1cea0b1e9dc2bca
CorneHQ <cornedev@outlook.com>
2021-03-26 15:44:06 +0100

    Added menu models

---
+++ Jack Darcy Restaurant/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jack_Darcy_Restaurant.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
